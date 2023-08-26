598d9c8caa819890bfcb7e3812d6023219602ae0
GabrielSilper <gabrielsilper.main@gmail.com>
2023-08-26 19:41:11 -0400

    refactor: agora o POST /users retorna um token válido por 1 dia.

--- src/entities/TokenPayload.ts
+++ src/entities/TokenPayload.ts
@@ -1,6 +1,5 @@
 export type TokenPayload = {
   _id: string;
-  name: string;
   role: string;
   email: string;
 };
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,21 +1,34 @@
 import { NewEntity } from '../entities';
 import IUser from '../entities/IUser';
 import ServiceData from '../entities/ServiceData';
+import { Token } from '../entities/Token';
+import TokenFunctions from '../entities/TokenFunctions';
 import CompanyModel from '../models/company.model';
 import UserModel from '../models/user.model';
 import httpStatus from 'http-status';
+import TokenJwt from '../utils/TokenJwt';
+import { TokenPayload } from '../entities/TokenPayload';
 
 export default class UserService {
   constructor(
     private userModel = UserModel,
-    private companyModel = CompanyModel
+    private companyModel = CompanyModel,
+    private jwtToken: TokenFunctions = new TokenJwt()
   ) {}
 
-  async create(newUser: NewEntity<IUser>): Promise<ServiceData<IUser>> {
+  async create(newUser: NewEntity<IUser>): Promise<ServiceData<Token>> {
     const createdUser = await this.userModel.create(newUser);
     await this.companyModel
       .find({ name: 'Workmize' })
       .updateOne({ $push: { users: createdUser._id } });
-    return { error: null, status: httpStatus.CREATED, data: createdUser };
+
+    const tokenPayload: TokenPayload = {
+      _id: createdUser._id,
+      email: createdUser.email,
+      role: createdUser.role,
+    };
+    const token = this.jwtToken.createToken(tokenPayload);
+
+    return { error: null, status: httpStatus.CREATED, data: { token } };
   }
 }
