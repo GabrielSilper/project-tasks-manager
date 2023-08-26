168cbb5d5737f65de22ac295c4d9b0237f0bbf63
GabrielSilper <gabrielsilper.main@gmail.com>
2023-08-26 19:27:38 -0400

    feature: middleware para validar os campos do body

---
+++ src/middlewares/ValidateUser.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from 'express';
+import httpStatus from 'http-status';
+
+export default class ValidateUSer {
+  static fields(req: Request, res: Response, next: NextFunction) {
+    const { body } = req;
+    const { name, password, image, email, role } = body;
+    if (!name || !password || !image || !email || !role) {
+      return res.status(httpStatus.BAD_REQUEST).send({
+        message: 'Missing required fields',
+      });
+    }
+    if (role !== 'admin' && role !== 'user') {
+      return res.status(httpStatus.UNPROCESSABLE_ENTITY).send({
+        message: 'Invalid role, must be "admin" or "user"',
+      });
+    }
+    return next();
+  }
+}
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 import UserController from '../controllers/user.controller';
+import ValidateUSer from '../middlewares/ValidateUser';
 
 const userRouter = express.Router();
 const userController = new UserController();
 
-userRouter.post('/', (req, res) => userController.create(req, res));
+userRouter.post(
+  '/',
+  (req, res, next) => ValidateUSer.fields(req, res, next),
+  (req, res) => userController.create(req, res)
+);
 
 export default userRouter;
