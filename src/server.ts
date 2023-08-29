4c844d93c1a796080144102de9efc58036ecace6
GabrielSilper <gabrielsilper.main@gmail.com>
2023-08-29 03:30:43 -0400

    PORT 3001

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import App from './app';
 
-const PORT = process.env.SERVER_PORT || 3001;
+const PORT = process.env.PORT || 3001;
 const DB_URI = process.env.DB_URI as string;
 
 const app = new App();
